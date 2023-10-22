bed5405659bd617d7e72f8d1252460c35d8accf2
psargeant <122289207+psargeant@users.noreply.github.com>
2023-10-22 17:30:14 -0400

    Early Work

---
+++ DDL.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Units(
+    unit_ID AUTO_INCREMENT UNIQUE NOT NULL
+    is_available BOOL NOT NULL DEFAULT(1)
+    num_bedrooms int not NULL
+    num_bathrooms int not NULL
+    square_feet int not NULL
+)
+
+
+
+
+
+
+
+INSERT 
