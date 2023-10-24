3ef3b1da572c2024ba816aeeabb2d491e5743249
psargeant <122289207+psargeant@users.noreply.github.com>
2023-10-24 19:39:13 -0400

    Fully imports need to rethink maintenance requests

--- DDL.sql
+++ DDL.sql
@@ -62,11 +62,11 @@ CREATE TABLE Tenants(
     PRIMARY KEY (tenant_ID)
 );
 
-INSERT INTO Tenants (first_name, last_name, phone_number, email, rent_balance, rental_agreement_id)
-VALUES ('Victoria', 'Jones', '1234567890', 'victoriajones@yahoo.com', 5000.00, 1),
-       ('Emma', 'Mathis', '5555555555', 'emmamathis@hotmail.com', 6000.00, 10),
-       ('John', 'Carey', '3127237777', 'john.carey@gmail.com', 4500.00, 33),
-       ('Eric', 'Williams', '3128388548', 'eric222@gmail.com', 5200.00, 22);
+INSERT INTO Tenants (first_name, last_name, phone_number, email, rent_balance)
+VALUES ('Victoria', 'Jones', '1234567890', 'victoriajones@yahoo.com', 5000.00),
+       ('Emma', 'Mathis', '5555555555', 'emmamathis@hotmail.com', 6000.00),
+       ('John', 'Carey', '3127237777', 'john.carey@gmail.com', 4500.00),
+       ('Eric', 'Williams', '3128388548', 'eric222@gmail.com', 5200.00);
 
 CREATE TABLE RentalAgreements(
     rental_ID int AUTO_INCREMENT UNIQUE NOT NULL,
@@ -81,15 +81,15 @@ CREATE TABLE RentalAgreements(
     FOREIGN KEY (tenant_ID) REFERENCES Tenants(tenant_ID)
 );
 
-INSERT INTO RentalAgreements (unit_ID, tenant_ID, start_date, end_date, total_rental_balance, security_deposit)
+INSERT INTO RentalAgreements (unit_ID, tenant_ID, start_date, end_date, total_rent_balance, security_deposit)
 VALUES
-    (SELECT unit_ID FROM Units WHERE unit_number = 101, SELECT tenant_ID FROM Tenants WHERE rental_agreement_id = 10, "20210901", "20220831", 9300.00, 1000.00),
-    (SELECT unit_ID FROM Units WHERE unit_number = 2010, SELECT tenant_ID FROM Tenants WHERE rental_agreement_id = 1, "20210112", "20220111", 9500.00, 1200.00),
-    (SELECT unit_ID FROM Units WHERE unit_number = 1020, SELECT tenant_ID FROM Tenants WHERE rental_agreement_id = 33, "20220823", "20230822", 7000.00, 1200.00),
-    (SELECT unit_ID FROM Units WHERE unit_number = 1030, SELECT tenant_ID FROM Tenants WHERE rental_agreement_id = 22, , "20220522", "20230501", 800.00, 1300.00);
+    ((SELECT unit_ID FROM Units WHERE unit_number = 101), (SELECT tenant_ID FROM Tenants WHERE first_name = 'Victoria' AND last_name = 'Jones'), "20210901", "20220831", 9300.00, 1000.00),
+    ((SELECT unit_ID FROM Units WHERE unit_number = 2010), (SELECT tenant_ID FROM Tenants WHERE first_name = 'Emma' AND last_name= 'Mathis'), "20210112", "20220111", 9500.00, 1200.00),
+    ((SELECT unit_ID FROM Units WHERE unit_number = 1020), (SELECT tenant_ID FROM Tenants WHERE first_name = 'John' AND last_name= 'Carey'), "20220823", "20230822", 7000.00, 1200.00),
+    ((SELECT unit_ID FROM Units WHERE unit_number = 1030), (SELECT tenant_ID FROM Tenants WHERE first_name = 'Eric' AND last_name= 'Williams'), "20220522", "20230501", 800.00, 1300.00);
     
 
-CREATE TABLE MaintenceWorkers(
+CREATE TABLE MaintenanceWorkers(
     worker_ID int AUTO_INCREMENT UNIQUE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -106,8 +106,8 @@ VALUES ('John', 'Anderson', '3221234567', 20.50, 'Electrician', 40.25),
 ('Robert', 'Miller', '3222345678', 28.00, 'HVAC service technician', 10.00),
 ('Sarah', 'Jones', '3228765432', 29.25, 'Elevator mechanic', 30.75);
 
-CREATE TABLE MaintenceRequests(
-    maintenence_request_ID int AUTO_INCREMENT UNIQUE NOT NULL,
+CREATE TABLE MaintenanceRequests(
+    maintenance_request_ID int AUTO_INCREMENT UNIQUE NOT NULL,
     unit_ID int NOT NULL,
     tenant_ID int not NULL,
     description TEXT NOT NULL,
@@ -115,30 +115,31 @@ CREATE TABLE MaintenceRequests(
     time_to_complete int NOT NULL,
     repair_cost DECIMAL(7,2) NOT NULL,
     is_closed BOOLEAN NOT NULL,
-    PRIMARY KEY (maintenence_request_ID),
+    PRIMARY KEY (maintenance_request_ID),
     FOREIGN KEY(unit_ID) REFERENCES Units(unit_ID),
     FOREIGN KEY (tenant_ID) REFERENCES Tenants(tenant_ID)
 );
 
 INSERT INTO MaintenanceRequests (unit_id, tenant_id, description, date_submitted, time_to_complete, repair_cost, is_closed)
-VALUES (SELECT unit_ID FROM Units WHERE unit_number = 101, SELECT tenant_ID FROM Tenants WHERE rental_agreement_id = 10, "Leaky faucet in bathroom", "20230810", 2, 50.00, 0),
-(SELECT unit_ID FROM Units WHERE unit_number = 2010, SELECT tenant_ID FROM Tenants WHERE rental_agreement_id = 1, 'Broken window in bathroom', "20221012", 3, 75.00, 0),
-(SELECT unit_ID FROM Units WHERE unit_number = 1030, SELECT tenant_ID FROM Tenants WHERE rental_agreement_id = 22, 'Heating system blowing cold air', "202310-5", 6, 200.00, 1),
-(SELECT unit_ID FROM Units WHERE unit_number = 1020, SELECT tenant_ID FROM Tenants WHERE rental_agreement_id = 33, 'Garbage disposal backing up', "20231018", 4, 150.00, 1);
+VALUES ((SELECT unit_ID FROM Units WHERE unit_number = 101), (SELECT tenant_ID FROM Tenants WHERE first_name = 'Victoria' AND last_name= 'Jones'), "Leaky faucet in bathroom", "20230810", 2, 50.00, 0),
+((SELECT unit_ID FROM Units WHERE unit_number = 2010), (SELECT tenant_ID FROM Tenants WHERE first_name = 'Emma' AND last_name= 'Mathis'), 'Broken window in bathroom', "20221012", 3, 75.00, 0),
+((SELECT unit_ID FROM Units WHERE unit_number = 1020), (SELECT tenant_ID FROM Tenants WHERE first_name = 'John' AND last_name= 'Carey'), 'Heating system blowing cold air', "202310-5", 6, 200.00, 1),
+((SELECT unit_ID FROM Units WHERE unit_number = 1030), (SELECT tenant_ID FROM Tenants WHERE first_name = 'Eric' AND last_name= 'Williams'), 'Garbage disposal backing up', "20231018", 4, 150.00, 1);
 
 
 CREATE TABLE RequestAssignments(
     assignmnet_ID int AUTO_INCREMENT UNIQUE NOT NULL,
     worker_ID int not NULL,
-    maintenence_request_ID int NOT NULL,
+    maintenance_request_ID int NOT NULL,
     PRIMARY KEY(assignmnet_ID),
-    FOREIGN KEY(worker_ID) REFERENCES MaintenceWorkers(worker_ID),
-    FOREIGN KEY (maintenence_request_ID) REFERENCES MaintenceRequests(maintenence_request_ID)
+    FOREIGN KEY(worker_ID) REFERENCES MaintenanceWorkers(worker_ID),
+    FOREIGN KEY (maintenance_request_ID) REFERENCES MaintenanceRequests(maintenance_request_ID)
 );
+
 
 INSERT INTO RequestAssignments (worker_ID, maintenance_request_ID) 
 VALUES 
-(SELECT worker_ID FROM MaintenanceWorkers WHERE first_name = 'John' and last_name= 'Anderson', SELECT maintenance_request_ID FROM MaintenanceRequests WHERE unit_number = 101),
-(SELECT worker_ID FROM MaintenanceWorkers WHERE first_name = 'Jane' and last_name= 'Brown', SELECT maintenance_request_ID FROM MaintenanceRequests WHERE unit_number = 2010),
-(SELECT worker_ID FROM MaintenanceWorkers WHERE first_name = 'Robert' and last_name= 'Miller', SELECT maintenance_request_ID FROM MaintenanceRequests WHERE unit_number = 1030),
-(SELECT worker_ID FROM MaintenanceWorkers WHERE first_name = 'Sarah' and last_name= 'Jones', SELECT maintenance_request_ID FROM MaintenanceRequests WHERE unit_number = 1020);
+((SELECT worker_ID FROM MaintenanceWorkers WHERE first_name = 'John' and last_name= 'Anderson'), 1),
+((SELECT worker_ID FROM MaintenanceWorkers WHERE first_name = 'Jane' and last_name= 'Brown'), 2),
+((SELECT worker_ID FROM MaintenanceWorkers WHERE first_name = 'Robert' and last_name= 'Miller'), 3),
+((SELECT worker_ID FROM MaintenanceWorkers WHERE first_name = 'Sarah' and last_name= 'Jones'), 4);
