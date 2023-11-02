15b142365403545f9051936fc05648171cc5f96a
Monika <raciam@oregonstate.edu>
2023-11-02 17:14:45 -0500

    Add form to Units

--- DMQ.sql
+++ DMQ.sql
@@ -7,13 +7,19 @@ SELECT * FROM Units;
 SELECT * FROM Tenants;
 
 --- Select all RentalAgreements
-SELECT * FROM RentalAgreements;
+SELECT RentalAgreements.rental_ID, Tenants.first_name, Tenants.last_name, Units.unit_number, RentalAgreements.start_date, RentalAgreements.end_date, RentalAgreements.total_rent_balance, RentalAgreements.security_deposit
+FROM RentalAgreements
+JOIN Tenants ON RentalAgreements.tenant_ID = Tenants.tenant_ID
+JOIN Units ON RentalAgreements.unit_ID = Units.unit_ID;
 
 --- Select all UtilityProviders
 SELECT * FROM UtilityProviders;
 
 --- Select all MaintenanceRequests
-SELECT * FROM MaintenanceRequests;
+SELECT MaintenanceRequests.maintenance_request_ID, Units.unit_number, Tenants.first_name, Tenants.last_name, MaintenanceRequests.description, MaintenanceRequests.date_submitted, MaintenanceRequests.time_to_complete, MaintenanceRequests.repair_cost, MaintenanceRequests.is_closed
+FROM MaintenanceRequests
+JOIN Units ON MaintenanceRequests.unit_ID = Units.unit_ID
+JOIN Tenants ON MaintenanceRequests.tenant_ID = Tenants.tenant_ID;
 
 --- Select all MaintenanceWorkers
 SELECT * FROM MaintenanceWorkers;
