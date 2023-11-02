3363b5c73f5fa54e63fe9e37c73e2a0407921853
Monika <raciam@oregonstate.edu>
2023-11-02 06:35:26 -0500

    Add queries; update tables

--- DMQ.sql
+++ DMQ.sql
@@ -1,7 +1,38 @@
---------------------- SELECT --------------------------
+------------------------- SELECT Queries --------------------------
 
---------------------- INSERT --------------------------
+--- Select all Units (in progress)
+SELECT * FROM Units;
+--- Select all Tenants (in progress)
+SELECT * FROM Tenants;
+--- Select all RentalAgreements
 
---------------------- UPDATE --------------------------
+--- Select all UtilityProviders
+SELECT * FROM UtilityProviders;
 
---------------------- DELETE --------------------------
+--- Select all MaintenanceRequests ---
+
+--- Select all MaintenanceWorkers ---
+
+-------------------------- INSERT Queries --------------------------
+
+--- Insert into Units (in progress)
+
+--- Insert into Tenants (in progress)
+
+--- Insert into RentalAgreements
+
+--- Insert into UtilityProviders
+INSERT INTO UtilityProviders (name, service_type, utility_cost)
+VALUES (:providerNameInput, :serviceTypeInput, :utilityCostInput);
+
+--- Insert into MaintenanceRequests
+
+--- Insert into MaintenanceWorkers
+
+
+-------------------------- UPDATE Queries --------------------------
+--- Update Unit (in progress)
+--- Update Tenant (in progress)
+
+-------------------------- DELETE Queries --------------------------
+--- Delete Tenant (in progress)
